5bb9441bdfc2e04fd94217af3ab610ee3ee1481a
Valerie Riley <valeriesriley@gmail.com>
2019-12-12 18:07:42 -0700

    added images of apps when mouse over on portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var happyTalesDiv = $('#happyTalesImage');
+var plannerDiv = $('#planner');
+var passwordGeneratorDiv = $('#passwordGenerator');
+
+happyTalesDiv.onmouseout = function(){
+    this.src = 'assets/images/HappyTales.jpg';
+};
